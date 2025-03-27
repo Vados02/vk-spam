8c0d17b3e26ce7adec18e22c1879cb0603fab24a
vados <89159662+Vados02@users.noreply.github.com>
2025-03-27 20:11:03 +0300

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,12 @@
+# Твой токен VK API (https://vkhost.github.io/)
+ACCESS_TOKEN = "vk1.a.e0SXwPeGv6tCeb8FEYLI84F1zCA3po3wHno8jbZcHdgnUyU1c7apbmNIpOjNiHgvnXqM12eTWRe3Mu4KQ8SKPoD0D7xb5FjSEq-LDQ6RoJpidAB-OEAE6GF2VA2yeNpkKIzkd1ZGrGwks2r99SPxBihQZFnr_htVvcLlTWJZh1R8ax9mmtOLGz2GPqSuUdxOtz85tZ6qRGkMs0hX9T2sVQ"  
+
+# Группы для публикации
+GROUPS = ["mihilson2090"]
+
+# Сообщения для публикации
+POST_MESSAGES = [
+    "https://dzen.ru/b/Z-QvKS3MeQ1JDGV7"
+]
+# Ссылка на изображение для поста
+BANNER_URL = "https://avatars.dzeninfra.ru/get-zen_brief/271828/pub_67e42f292dcc790d490c657b_67e435bbffee86444fc0961e/scale_1200"  # Ссылка на изображение
